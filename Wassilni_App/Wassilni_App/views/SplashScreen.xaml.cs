dbd43a2d8ef11c13b60ad80d534d23fdf0369c1a
Farha Dahman <farhajehad@gmail.com>
2023-04-02 17:05:21 +0300

    adding the Splash Screen

---
+++ Wassilni_App/Wassilni_App/views/SplashScreen.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wassilni_App.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashScreen : ContentPage
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await Task.Delay(6000);
+            await Navigation.PushAsync(new LoginPage());
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
