e0e83f02d17812aecafa92cee850afabce1993f7
Farha Dahman <farhajehad@gmail.com>
2023-04-03 17:16:22 +0300

    adding Login via google feature

--- Wassilni_App/Wassilni_App/views/LoginPage.xaml.cs
+++ Wassilni_App/Wassilni_App/views/LoginPage.xaml.cs
@@ -1,26 +1,61 @@
-﻿using System;
+﻿using Wassilni_App.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Wassilni_App.viewModels;
+using Wassilni_App.views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Android.App;
+
 
 namespace Wassilni_App.views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private readonly IGoogleManager _googleManager;
+        GoogleUser GoogleUser = new GoogleUser();
+        public bool IsLogedIn { get; set; }
+
         public LoginPage()
         {
             InitializeComponent();
+            _googleManager = DependencyService.Get<IGoogleManager>();
             this.BindingContext = new LoginViewModel();
         }
 
-        async private void GoToForgetPassPage(object sender, EventArgs e)
+        private void btnLogin_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new ForgetPasswordPage());
+            _googleManager.Login(OnLoginComplete);
+
+        }
+        private void OnLoginComplete(GoogleUser googleUser, string message)
+        {
+            if (googleUser != null)
+            {
+
+                IsLogedIn = true;
+                App.Current.MainPage = new NavigationPage(new TabbedBottom());
+
+            }
+            else
+            {
+                DisplayAlert("Message", message, "Ok");
+            }
+        }
+        private void GoogleLogout()
+        {
+            _googleManager.Logout();
+            IsLogedIn = false;
+        }
+        private void btnLogout_Clicked(object sender, EventArgs e)
+        {
+            _googleManager.Logout();
+
         }
 
         async private void GoToTheHomePage(object sender, EventArgs e)
@@ -33,9 +68,6 @@ namespace Wassilni_App.views
             await Navigation.PushAsync(new SignupPage());
         }
 
-
-
-
-
     }
+
 }
