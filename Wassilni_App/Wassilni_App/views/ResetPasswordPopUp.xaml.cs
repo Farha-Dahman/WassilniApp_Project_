4c604e021325091a8e33885e6184b57d5156815e
“Nama-Salameh <92352860+Nama-Salameh@users.noreply.github.com>
2023-04-03 18:25:08 +0300

    Design for reset password pop up page.

---
+++ Wassilni_App/Wassilni_App/views/ResetPasswordPopUp.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wassilni_App.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResetPasswordPopUp : ContentPage
+    {
+        public ResetPasswordPopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
