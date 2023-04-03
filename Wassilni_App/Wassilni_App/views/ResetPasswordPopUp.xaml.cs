dd92c8ddd48c7a1bc14d357f785b182e1c010e79
“Nama-Salameh <92352860+Nama-Salameh@users.noreply.github.com>
2023-04-03 18:30:55 +0300

    Reset password pop up

--- Wassilni_App/Wassilni_App/views/ResetPasswordPopUp.xaml.cs
+++ Wassilni_App/Wassilni_App/views/ResetPasswordPopUp.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,19 @@ using Xamarin.Forms.Xaml;
 namespace Wassilni_App.views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ResetPasswordPopUp : ContentPage
+    public partial class ResetPasswordPopUp : Rg.Plugins.Popup.Pages.PopupPage
     {
         public ResetPasswordPopUp()
         {
             InitializeComponent();
+        }
+        async private void Ok_Clicked(object sender, EventArgs e)
+        {
+            // go to the login page
+            await Navigation.PushAsync(new LoginPage());
+            //close the pop up page
+            await PopupNavigation.Instance.PopAsync();
+
         }
     }
 }
