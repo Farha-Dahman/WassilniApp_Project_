e08c3362ebeef4acc5f924a591340db509719eb8
ahmadamer <s12011017@stu.najah.edu>
2023-04-07 18:58:17 +0200

    git commit -m "WA-21 <Create Pool Input Validation and Creation >"

--- Wassilni_App/Wassilni_App/views/CreatePoolPage.xaml.cs
+++ Wassilni_App/Wassilni_App/views/CreatePoolPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Android.Text.Format;
+using Firebase.Auth;
+using Java.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wassilni_App.Models;
 using Wassilni_App.viewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,10 +16,60 @@ namespace Wassilni_App.views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CreatePoolPage : ContentPage
     {
-        public CreatePoolPage()
+        private readonly string _userId;
+        private List<Frame> errorFrames;
+        private bool isErrorMessageVisible = false;
+
+
+        public CreatePoolPage(string id)
         {
             InitializeComponent();
-            this.BindingContext = new CreatePoolViewModel();
+            _userId = id;
+
+            this.BindingContext = new CreatePoolViewModel(_userId);
+
+            var viewModel = BindingContext as CreatePoolViewModel;
+            viewModel.ShowTopErrorMessage += ViewModel_ShowTopErrorMessage;
         }
+        private async void ViewModel_ShowTopErrorMessage(object sender, EventArgs e)
+        {
+            TopErrorFrame.Opacity = 0;
+            TopErrorFrame.TranslationY = -30;
+            TopErrorFrame.IsVisible = true;
+
+            uint duration = 500;
+            uint shakeDuration = 50;
+            int shakeCount = 5;
+            double shakeOffset = 5;
+
+            await Task.WhenAll(
+                TopErrorFrame.FadeTo(1, duration, Easing.CubicInOut),
+                TopErrorFrame.TranslateTo(0, 0, duration, Easing.CubicInOut)
+            );
+
+
+            for (int i = 0; i < shakeCount; i++)
+            {
+                await Task.WhenAll(
+                    TopErrorFrame.TranslateTo(shakeOffset, 0, shakeDuration, Easing.Linear),
+                    TopErrorFrame.TranslateTo(-shakeOffset, 0, shakeDuration, Easing.Linear)
+                );
+            }
+            await TopErrorFrame.TranslateTo(0, 0, shakeDuration, Easing.Linear);
+
+
+            await Task.Delay(3000);
+
+
+            await Task.WhenAll(
+                TopErrorFrame.FadeTo(0, duration, Easing.CubicInOut),
+                TopErrorFrame.TranslateTo(0, -30, duration, Easing.CubicInOut)
+            );
+            TopErrorFrame.IsVisible = false;
+        }
+
+
+
+
     }
 }
