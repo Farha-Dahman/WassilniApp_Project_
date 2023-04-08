0ed2ffac636a44f92f127c49eceebab6c6d82703
“Nama-Salameh <92352860+Nama-Salameh@users.noreply.github.com>
2023-04-08 03:09:04 +0300

    WA-62 build the profile page

--- Wassilni_App/Wassilni_App/views/ProfilePage.xaml.cs
+++ Wassilni_App/Wassilni_App/views/ProfilePage.xaml.cs
@@ -5,25 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using Wassilni_App.viewModels;
 using Wassilni_App.views.Settings;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace Wassilni_App.views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
-        public ProfilePage()
+        public ProfilePage(String UserId)
         {
             InitializeComponent();
-            this.BindingContext = new ProfileViewModel();
+            this.BindingContext = new ProfileViewModel(UserId);
+            Settings.Clicked += GoToSettingsPage;
         }
-
         async private void GoToSettingsPage(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SettingsPage());
 
         }
-
     }
 }
