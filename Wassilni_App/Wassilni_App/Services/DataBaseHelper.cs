ccbf3ae6686a230bf547155ef9925d28ddd4e0ac
Farha Dahman <farhajehad@gmail.com>
2023-04-10 08:24:16 +0300

    WA-24 building the find pool feature

---
+++ Wassilni_App/Wassilni_App/Services/DataBaseHelper.cs
@@ -0,0 +1,59 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wassilni_App.Models;
+
+namespace Wassilni_App.Services
+{
+    public class DatabaseHelper
+    {
+        private readonly FirebaseClient _firebaseClient;
+
+        public DatabaseHelper(string firebaseUrl)
+        {
+            _firebaseClient = new FirebaseClient(firebaseUrl);
+        }
+
+        public async Task<List<Ride>> GetRidesAsync()
+        {
+            var rides = await _firebaseClient.Child("Ride").OnceAsync<Ride>();
+            return rides.Select(r => new Ride
+            {
+                StartLocation = r.Object.StartLocation,
+                EndLocation = r.Object.EndLocation,
+                PickupDateTime = r.Object.PickupDateTime,
+                Number_of_seats = r.Object.Number_of_seats,
+                DriverName = r.Object.DriverName,
+                PhoneNumber = r.Object.PhoneNumber,
+                PricePerRide = r.Object.PricePerRide,
+                PhotoUrl = r.Object.PhotoUrl,
+            }).ToList();
+        }
+
+        public async Task<List<Ride>> GetMatchingPoolsAsync(Ride pool)
+        {
+            var allRides = await GetRidesAsync();
+            return allRides.Where(r =>
+                r.StartLocation == pool.StartLocation &&
+                r.EndLocation == pool.EndLocation &&
+
+                r.Number_of_seats >= pool.Number_of_seats
+               )
+                  .Select(r => new Ride
+                  {
+                      PhotoUrl = r.PhotoUrl,
+                      StartLocation = r.StartLocation,
+                      EndLocation = r.EndLocation,
+                      PickupDateTime = r.PickupDateTime,
+                      Number_of_seats = r.Number_of_seats,
+                      DriverName = r.DriverName,
+                      PhoneNumber = r.PhoneNumber,
+                      PricePerRide = r.PricePerRide
+                  })
+            .ToList();
+        }
+
+    }
+}
--- Wassilni_App/Wassilni_App/views/FindPoolPage.xaml.cs
+++ Wassilni_App/Wassilni_App/views/FindPoolPage.xaml.cs
@@ -1,21 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Wassilni_App.viewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Wassilni_App.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Wassilni_App.Services;
+using System;
+using Wassilni_App.viewModels;
 
 namespace Wassilni_App.views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FindPoolPage : ContentPage
     {
-        public FindPoolPage()
+        public ObservableCollection<Ride> MatchingPools { get; set; }
+
+        public FindPoolPage(List<Ride> matchingPools)
         {
+
+
             InitializeComponent();
             this.BindingContext = new FindPoolViewModel();
+            MatchingPools = new ObservableCollection<Ride>(matchingPools);
+            PoolsCollectionView.ItemsSource = MatchingPools;
+        }
+        private async void OnItemTapped(object sender, EventArgs e)
+        {
+            var tappedFrame = sender as Frame;
+            var tappedItem = tappedFrame.BindingContext as Ride;
+
+            await Navigation.PushAsync(new MyTripsPage());
         }
     }
 }
