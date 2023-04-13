9aa00cf590af1ff08fde1e27a517f627960cac16
ahmadamer <s12011017@stu.najah.edu>
2023-04-13 23:16:55 +0200

    WA-79 Desing accept and denay popups

---
+++ Wassilni_App/Wassilni_App/views/PopUpSuccessRequest.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content.Res;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Wassilni_App.viewModels;
+using Wassilni_App.views.Settings;
+using Wassilni_App.views;
+using Rg.Plugins.Popup.Services;
+
+namespace Wassilni_App
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopUpSuccessRequest : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public PopUpSuccessRequest()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
