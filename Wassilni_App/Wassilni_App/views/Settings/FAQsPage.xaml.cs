33a56d09cdf765a7882adf97f14695c3ae1c1802
Farha Dahman <farhajehad@gmail.com>
2023-04-15 16:10:46 +0300

    WA-59 adding the FAQs page design

--- Wassilni_App/Wassilni_App/views/Settings/FAQsPage.xaml.cs
+++ Wassilni_App/Wassilni_App/views/Settings/FAQsPage.xaml.cs
@@ -20,6 +20,21 @@ namespace Wassilni_App.views.Settings
         private void OnSourceFrameTapped(object sender, EventArgs e)
         {
             Answer_one.IsVisible = !Answer_one.IsVisible;
+            arrow_1.Text = Answer_one.IsVisible ? "\uf077" : "\uf078";
         }
+        private void OnSourceFrameTapped2(object sender, EventArgs e)
+        {
+            Answer_two.IsVisible = !Answer_two.IsVisible;
+            arrow_2.Text = Answer_two.IsVisible ? "\uf077" : "\uf078";
+        }
+        private void OnSourceFrameTapped3(object sender, EventArgs e)
+        {
+            Answer_three.IsVisible = !Answer_three.IsVisible;
+            arrow_3.Text = Answer_three.IsVisible ? "\uf077" : "\uf078";
+        }
+
+
+
+
     }
 }
