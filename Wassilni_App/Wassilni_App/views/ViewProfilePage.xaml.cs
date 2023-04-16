bb3f4f448210b836571383d1fbd5782d4fdee310
“Nama-Salameh <92352860+Nama-Salameh@users.noreply.github.com>
2023-04-16 15:12:22 +0300

    WA-85 Build view profile page.

---
+++ Wassilni_App/Wassilni_App/views/ViewProfilePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wassilni_App.viewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wassilni_App.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewProfilePage : ContentPage
+    {
+        public ViewProfilePage(String email)
+        {
+            InitializeComponent();
+            BindingContext = new ViewProfilePageViewModel(email);
+        }
+    }
+}
