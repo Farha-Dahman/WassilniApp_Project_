05f47adc03c97e6af2a1eaca40cf4246f8adfbce
“Nama-Salameh <92352860+Nama-Salameh@users.noreply.github.com>
2023-04-16 15:21:27 +0300

    WA-83 Build search feature.

---
+++ Wassilni_App/Wassilni_App/views/SearchPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Wassilni_App.viewModels;
+namespace Wassilni_App.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        SearchViewModel viewModel;
+
+        public SearchPage()
+        {
+            InitializeComponent();
+            BindingContext = new SearchViewModel();
+        }
+
+    }
+}
