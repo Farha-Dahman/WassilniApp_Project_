49b6e94a39531842d464ba63fa668b83f9c4a923
Farha Dahman <farhajehad@gmail.com>
2023-04-18 05:05:46 +0300

    WA-59 edit the FAQs page design

--- Wassilni_App/Wassilni_App/views/Settings/FAQsPage.xaml.cs
+++ Wassilni_App/Wassilni_App/views/Settings/FAQsPage.xaml.cs
@@ -32,7 +32,21 @@ namespace Wassilni_App.views.Settings
             Answer_three.IsVisible = !Answer_three.IsVisible;
             arrow_3.Text = Answer_three.IsVisible ? "\uf077" : "\uf078";
         }
-
+        private void OnSourceFrameTapped4(object sender, EventArgs e)
+        {
+            Answer_four.IsVisible = !Answer_four.IsVisible;
+            arrow_4.Text = Answer_four.IsVisible ? "\uf077" : "\uf078";
+        }
+        private void OnSourceFrameTapped5(object sender, EventArgs e)
+        {
+            Answer_five.IsVisible = !Answer_five.IsVisible;
+            arrow_5.Text = Answer_five.IsVisible ? "\uf077" : "\uf078";
+        }
+        private void OnSourceFrameTapped6(object sender, EventArgs e)
+        {
+            Answer_six.IsVisible = !Answer_six.IsVisible;
+            arrow_6.Text = Answer_six.IsVisible ? "\uf077" : "\uf078";
+        }
 
 
 
