4038fac229ff96add3573ce521f5e747e356b8b5
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-04-25 20:51:30 +0300

    modify the aleart of the reset password

---
+++ Wassilni_App/Wassilni_App/views/Settings/ChangePasswordPopUp.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms.Xaml;
+
+namespace Wassilni_App.views.Settings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChangePasswordPopUp : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public ChangePasswordPopUp()
+        {
+            InitializeComponent();
+        }
+
+        private async void Ok_Clicked(object sender, System.EventArgs e)
+        {
+           
+            await Navigation.PushAsync(new ChangePasswordPage());
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
